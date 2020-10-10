725ca47077d58caa7f5e0e22ab60fbc4b9f3546a
mnswdhw <mnswdhw@gmail.com>
2020-10-10 22:55:03 +0530

    fixed ip address and location functionality of main app,implemented news page data functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Cricket from './cricket';
@@ -8,28 +8,71 @@ import Movie from './movie'
 import Footer from './footer'
 import Header from './header'
 import IP from "./ip"
+import { getDefaultNormalizer } from '@testing-library/react';
 const axios = require("axios");
 
 
 var res1;
-axios.get("https://api.ipify.org").then(res => {  
-res1 = res.data ;
-IP(res1);
-});
+// let output;
 
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Manas Branch </h1>
-      <Cricket/>
-      <News/>
-      <Food/>
-      <Movie/>
-      <Footer/>
-      <Header/>
-    </div>
-  );
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [output,setOutput]=useState({});
+  var country;
+  useEffect(()=> {
+   
+    axios.get("https://api.ipify.org").then(res => {  
+    res1 = res.data ;
+
+    IP(res1).then(function(response){
+       
+      
+      
+       setOutput(response);
+       setIsLoaded(true);
+       
+       
+       
+       
+     });
+
+    })
+
+  },[])
+ 
+  if(!isLoaded)
+  {
+    return <div>Loading..</div>
+  }
+  else{
+    return (
+
+    
+
+      <div className="App">
+       
+        
+    <h1>{console.log(output)}Manas Branch {output.data.country} </h1>
+        {/* <Cricket/> */}
+        <News location={output.data.country}/>
+        {/* <Food/>
+        <Movie/>
+        <Footer/>
+        <Header/> */}
+      </div>
+    );
+
+  }
+  
+
+
+
+  
 }
 
 export default App;
+
+
+
+
--- src/ip.js
+++ src/ip.js
@@ -1,16 +1,11 @@
 
 const axios = require("axios");
-export default function IP(ip){
-   let response;
-    let endpoint="http://ip-api.com/json/"+ip;
-    (async () => {
-         response = await axios({
-          url: endpoint,
-          method: 'get'
-        })
-      
-        console.log(response);
-      })()
-    return response;
+
+
+
+export default function IP(ip)
+{
+  return axios.get("http://ip-api.com/json/"+ip);
+  
 }
 
